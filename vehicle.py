6e48e974a1cac9ccb2cea2ebf2ef32d69b17d98a
Aradhy2004 <gaurish865@gmail.com>
2026-01-07 12:24:38 +0530

    UC2: Encapsulation and security using private attributes

---
+++ vehicle.py
@@ -0,0 +1,43 @@
+class Vehicle:
+    """
+    Represents a vehicle in the Eco-Ride fleet
+    """
+
+    def __init__(self, vehicle_id, model, battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.__battery_percentage = None
+        self.__maintenance_status = "OK"
+        self.__rental_price = 0.0
+        self.set_battery_percentage(battery_percentage)
+
+    # Battery Percentage
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+
+    def set_battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            raise ValueError("Battery percentage must be between 0 and 100")
+
+    # Maintenance Status
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    # Rental Price
+    def get_rental_price(self):
+        return self.__rental_price
+
+    def set_rental_price(self, price):
+        if price >= 0:
+            self.__rental_price = price
+        else:
+            raise ValueError("Rental price cannot be negative")
+
+        
+        
+
